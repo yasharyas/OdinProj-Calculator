df09a7d9fe19728ba38defb899e490b49b4c396f
yasharyas <yasharyas@proton.me>
2024-10-29 19:52:07 +0530

    Implement operate function to handle operations

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function multiply(a, b) {
 function divide(a, b) {
 	return b === 0 ? "Error" : a / b;
 }
+function operate(operator, a, b) {
+	switch (operator) {
+		case "+":
+			return add(a, b);
+		case "-":
+			return subtract(a, b);
+		case "*":
+			return multiply(a, b);
+		case "/":
+			return divide(a, b);
+		default:
+			return null;
+	}
+}
